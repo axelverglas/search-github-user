9dc96857f2c385603003ea36f0771ff29b09a3d2
axel <axel@rockusupport.com>
2023-02-21 17:46:12 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import TopArea from "./components/TopArea";
+
+export default function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center py-12 px-10 md:px-28">
+      <TopArea />
+    </div>
+  );
+}
